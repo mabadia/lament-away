8e1c3974e01782351209536bfa28f972cab8082a
mabadia <angel.beyou95@gmail.com>
2023-12-10 21:33:55 -0800

    add: login page add: current user page update: app.js page to link all routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import SignUpForm from './SignUpForm';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import signUp from './signUp';
+import Login from './users/Login';
+import CurrentUserProvider from './contexts/CurrentUser'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <SignUpForm />
-    </div>
+    <CurrentUserProvider>
+      <Router>
+        <Switch>
+          <Route path="/signUp" component={signUp} />
+          <Route path="/Login" component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
+      </Router>
+    </CurrentUserProvider>
   );
 }
 

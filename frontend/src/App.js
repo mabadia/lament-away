1406a6b8d9e4e8fd94459ea5c5be4e42b4e9aaf2
lbayley512 <lbayley512@gmail.com>
2023-12-11 19:59:55 -0800

    Edit Job posing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 import Navigation from './Navigation'
 import Home from './Home'
+import EditPlaceForm from './jobs/editJobForm';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
      <Navigation/>
       <Routes>
         <Route exact path="/" element={<Home />}/>
+        <Route exact path="/editJob" element={<EditPlaceForm />}/>
       </Routes>
      
      </>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -10,8 +10,10 @@ function Home() {
         <div className='home'>
           <>
           <h2>Rate Your Job Experience Today!</h2>
-            <Button variant="secondary">Secondary</Button>{''}
+          <div className='search'>
+            <Button variant="secondary" id="search_btn">Search</Button>{''}
             <input type="text" placeholder="Search For Companies Here" id="search"/>
+          </div>
           </> 
         </div>
       

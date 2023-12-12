a1668c57b0e3f5ac04871106e4442c9a7cfde1bd
mabadia <angel.beyou95@gmail.com>
2023-12-11 21:34:21 -0800

    updated; app function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './Navigation';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import signUp from './signUp';
+import signUp from './users/signUp';
 import Login from './users/Login';
 import CurrentUserProvider from './contexts/CurrentUser'
 import Home from './Home'
@@ -10,26 +9,22 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-    <>
-     <Navigation/>
-      <Routes>
-        <Route exact path="/" element={<Home />}/>
-      </Routes>
-     
-     </>
-    </BrowserRouter>
-    
-  //   <CurrentUserProvider>
-  //   <Router>
-  //     <Switch>
-  //       <Route path="/signUp" component={signUp} />
-  //       <Route path="/Login" component={Login} />
-  //       <Route path="/home" component={Home} />
-  //     </Switch>
-  //   </Router>
-  // </CurrentUserProvider>
-    );
+    <CurrentUserProvider>
+      <BrowserRouter>
+        <>
+          <Navigation />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/signUp" component={signUp} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/home" component={Home} />
+          </Routes>
+
+        </>
+      </BrowserRouter>
+    </CurrentUserProvider>
+
+  );
 }
 
 export default App;

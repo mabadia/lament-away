3b819122e2b3f2e7ca856a44e1a17ba294438b28
mabadia <angel.beyou95@gmail.com>
2023-12-11 20:36:55 -0800

    edit: error page

--- frontend/src/Error404.js
+++ frontend/src/Error404.js
@@ -1,14 +1,14 @@
-const React = require('react');
-const Def = require('./Default');
 
 function Error404() {
     return (
-        <Def>
+        
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Error, page not found</p>
                 <img src="" />
             </main>
-        </Def>
+      
     )
 }
+
+export default Error404;

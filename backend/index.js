cdfa9e41232baab61e09290f4838ba75a64d792b
mabadia <angel.beyou95@gmail.com>
2023-12-13 20:01:22 -0800

    add: folders and files in back end, routes moved: file NewBusinessForm.js into new folder

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
+//default route
+app.get('/', (req, res) => {
+    res.send('Welcome to the backend!');
+  });
+
 // Listen for Connections
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)

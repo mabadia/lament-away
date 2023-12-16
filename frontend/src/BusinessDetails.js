be69fcabeea6e2af7e778172b5753edd93f73d38
ronniebeezy <ronaldking577@yahoo.com>
2023-12-16 11:57:34 -0800

    Refactored and made the code more DRY. Cleaned up the structure and formatting for better readability.

--- frontend/src/BusinessDetails.js
+++ frontend/src/BusinessDetails.js
@@ -1,117 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import CommentCard from './CommentCard'; // Import CommentCard component
-import NewCommentForm from './NewCommentForm'; // Import NewCommentForm component
+import CommentCard from './CommentCard';
+import NewCommentForm from './NewCommentForm';
 
 function BusinessDetails() {
-  const { jobsId } = useParams();
-  const history = useHistory();
+  const { jobsId } = useParams(), history = useHistory();
   const [job, setJob] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/jobs/${jobsId}`);
-        const jobData = await response.json();
-        setJob(jobData);
-      } catch (error) {
-        console.error('Error fetching job details:', error);
-      }
-    };
+  useEffect(() => { fetch(`http://localhost:3000/jobs/${jobsId}`).then(r => r.json()).then(data => setJob(data)); }, [jobsId]);
 
-    fetchData();
-  }, [jobsId]);
+  if (!job) return <h1>Loading</h1>;
 
-  if (job === null) {
-    return <h1>Loading</h1>;
-  }
+  const editPlace = () => history.push(`/jobs/${job.jobsId}/edit`);
+  const deletePlace = async () => { await fetch(`http://localhost:3000/jobs/${job.jobsId}`, { method: 'DELETE' }); history.push('/jobs'); };
+  const deleteComment = async (c) => { await fetch(`http://localhost:3000/jobs/${job.jobsId}/comments/${c.commentId}`, { method: 'DELETE' }); setJob(p => ({ ...p, reviews: p.reviews.filter(cm => cm.commentId !== c.commentId) })); };
+  const createComment = async (cAttrs) => { const r = await fetch(`http://localhost:3000/jobs/${job.jobsId}/comments`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(cAttrs) }); const comment = await r.json(); setJob(p => ({ ...p, reviews: [...p.reviews, comment] })); };
 
-  const editPlace = () => {
-    history.push(`/jobs/${job.jobsId}/edit`);
-  };
+  const sumRatings = job.reviews.reduce((tot, c) => tot + c.stars, 0);
+  const averageRating = Math.round(sumRatings / job.reviews.length);
+  const stars = '⭐️'.repeat(averageRating);
 
-  const deletePlace = async () => {
-    try {
-      await fetch(`http://localhost:3000/jobs/${job.jobsId}`, {
-        method: 'DELETE',
-      });
-      history.push('/jobs');
-    } catch (error) {
-      console.error('Error deleting job:', error);
-    }
-  };
-
-  const deleteComment = async (deletedComment) => {
-    try {
-      await fetch(`http://localhost:3000/jobs/${job.jobsId}/comments/${deletedComment.commentId}`, {
-        method: 'DELETE',
-      });
-
-      setJob((prevJob) => ({
-        ...prevJob,
-        reviews: prevJob.reviews.filter((comment) => comment.commentId !== deletedComment.commentId),
-      }));
-    } catch (error) {
-      console.error('Error deleting comment:', error);
-    }
-  };
-
-  const createComment = async (commentAttributes) => {
-    try {
-      const response = await fetch(`http://localhost:3000/jobs/${job.jobsId}/comments`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(commentAttributes),
-      });
-
-      const comment = await response.json();
-
-      setJob((prevJob) => ({
-        ...prevJob,
-        reviews: [...prevJob.reviews, comment],
-      }));
-    } catch (error) {
-      console.error('Error creating comment:', error);
-    }
-  };
-
-  let reviews = (
-    <h3 className="inactive">
-      No user reviews yet!
-    </h3>
-  );
-  let rating = (
-    <h3 className="inactive">
-      This job location has not yet been rated
-    </h3>
-  );
-  let comments = [];
-
-  if (job.reviews.length) {
-    const sumRatings = job.reviews.reduce((total, c) => total + c.stars, 0);
-    const averageRating = Math.round(sumRatings / job.reviews.length);
-    let stars = '';
-
-    for (let i = 0; i < averageRating; i++) {
-      stars += '⭐️';
-    }
-
-    rating = (
-      <h3>
-        {stars} stars
-      </h3>
-    );
-
-    comments = job.reviews.map((comment) => (
-      <CommentCard key={comment.commentId} comment={comment} onDelete={() => deleteComment(comment)} />
-    ));
-  }
+  const reviews = job.reviews.length ? job.reviews.map(c => <CommentCard key={c.commentId} comment={c} onDelete={() => deleteComment(c)} />) : <h3 className="inactive">No user reviews yet!</h3>;
+  const rating = job.reviews.length ? <h3>{stars} stars</h3> : <h3 className="inactive">This job location has not yet been rated</h3>;
 
   return (
     <main>
-      {/* ... (your existing JSX) ... */}
+      <div className="row">
+        <div className="col-sm-6">
+          <img style={{ maxWidth: 200 }} src={job.pic} alt={job.name} />
+          <h3>Located in {job.city}, {job.state}</h3>
+        </div>
+        <div className="col-sm-6">
+          <h1>{job.name}</h1>
+          <h2>Rating</h2>
+          {rating}
+          <br />
+          <h2>Description</h2>
+          <h3>{job.name} This store location has been located here {job.city}, {job.state} since {job.founded}.</h3>
+          <a className="btn btn-warning" onClick={editPlace}>Edit</a>{' '}
+          <button type="submit" className="btn btn-danger" onClick={deletePlace}>Delete</button>
+        </div>
+      </div>
+      <hr />
+      <h2>Comments</h2>
+      <div className="row">{reviews}</div>
+      <hr />
+      <h2>Leave your review of this stores location?</h2>
+      <NewCommentForm place={job} onSubmit={createComment} />
     </main>
   );
 }

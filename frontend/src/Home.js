d2621c6248f23983b2180b3dbda5901d24174709
mabadia <angel.beyou95@gmail.com>
2023-12-19 19:47:07 -0800

    delete: removed indents

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -7,19 +7,17 @@ function Home() {
     <>
       <main>
         <h1>Lament-Away</h1>
-          <div className='home-container'>
-             <>
-             <h2>Rate Your Job Experience Today!</h2>
-             <div className='search'>
+        <div className='home-container'>
+          <>
+            <h2>Rate Your Job Experience Today!</h2>
+            <div className='search'>
               <Button variant="secondary" id="search_btn">Search</Button>{''}
               <input type="text" placeholder="Search For Companies Here" id="search" />
             </div>
           </>
-          <div className='static-box'>   
-                    
-                    <h3> For You Page </h3>
-
-                  </div>
+          <div className='static-box'>
+            <h3> For You Page </h3>
+          </div>
         </div>
 
 

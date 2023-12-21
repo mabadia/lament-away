a2e358f798042d5ff56a6c2d31f17ad37b313a08
mabadia <angel.beyou95@gmail.com>
2023-12-20 18:51:08 -0800

    add: home page footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import BusinessIndex from './jobs/BusinessIndex'
 import BusinessDetails from './jobs/BusinessDetails'
 import Navigation from './Navigation'
 import Error404 from './Error404'
-import NewPlaceForm from './jobs/NewBusinessForm'
+import NewBusinessForm from './jobs/NewBusinessForm'
 import EditBusinessForm from './jobs/EditBusinessForm'
 import SignUpForm from './users/SignUpForm'
 import LoginForm from './users/LoginForm'
 import CurrentUserProvider from './contexts/CurrentUser'
+import './app.css'
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
             <Route exact path="/login" component={LoginForm} />
             <Route exact path="/signUp" component={SignUpForm} />
             <Route exact path="/jobs" component={BusinessIndex} />
-            <Route exact path="/jobs/new" component={NewPlaceForm} />
+            <Route exact path="/jobs/new" component={NewBusinessForm} />
             <Route exact path="/jobs/:businessId" component={BusinessDetails} />
             <Route exact path="/jobs/:businessId/edit" component={EditBusinessForm} />
             <Route path="/" component={Error404} />
